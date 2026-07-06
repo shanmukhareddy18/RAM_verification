7b307a36ffe3dae8dc5c9a9e98ae2ac1dc4cb1d7
Reddygari Shanmukha reddy <shanmukhareddyshan@gmail.com>
2026-07-06 22:15:05 +0530

    Create generator.sv

---
+++ src/testbench/generator.sv
@@ -0,0 +1,15 @@
+class ram_generator;
+   ram_transaction blueprint;
+  mailbox#(ram_transaction)mbx_gd;
+  function new(mailbox#(ram_transaction)mbx_gd);
+    this.mbx_gd=mbx_gd;
+    blueprint=new();
+  endfunction
+  task start();
+    for(int i=0;i<`num_transactions;i++)begin
+        assert(blueprint.randomize());
+        mbx_gd.put(blueprint.copy());
+      $display("GENERATOR: data_in=%0h,write_enb=%0d,read_enb=%0d,address=%0h",blueprint.data_in,blueprint.write_enb,blueprint.read_enb,blueprint.address,$time);
+    end
+  endtask
+endclass
