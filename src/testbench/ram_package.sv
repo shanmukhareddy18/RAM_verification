9133ab82283958bb14fc594f0a9eb12039a5059a
Reddygari Shanmukha reddy <shanmukhareddyshan@gmail.com>
2026-07-06 22:48:41 +0530

    Create ram_package.sv

---
+++ src/testbench/ram_package.sv
@@ -0,0 +1,11 @@
+package ram_package;
+`include "defines.svh"
+`include "transaction.sv"
+`include "generator.sv"
+`include "driver.sv"
+`include "monitor.sv"
+`include "reference.sv"
+`include "scoreboard.sv"
+`include "environment.sv"
+`include "test.sv"
+endpackage
